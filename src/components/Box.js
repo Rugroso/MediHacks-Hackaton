f89590cb77dc13d91dc78d7ba192150e32c44f0b
inaki1010 <inakiperez003@gmail.com>
2023-09-09 21:46:24 -0700

    bg img

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
 import theme from '../theme';
 
-export default function Box({title, width, height}){
+export default function Box({title, width, height, img}){
+    const image = {uri: img};
+
+
     return(
         <View style={{width: width, height: height, backgroundColor: theme.colors.block, borderRadius: 30, alignItems: 'center'}}>
+             <ImageBackground source={image} resizeMode="cover" imageStyle={{borderRadius: 30}} style={{flex:1, width:'100%' }}>
             <Text>{title}</Text>
+            </ImageBackground>
         </View>
     )
 }
